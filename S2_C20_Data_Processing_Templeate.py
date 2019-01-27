4d0398fe4c6db4be9c2edb5146a2ff978f7b06ee
didierr <didier.rda@gmail.com>
2019-01-26 22:03:54 -0200

    S2_C20_Data_Processing_Templeate.py created

---
+++ S2_C20_Data_Processing_Templeate.py
@@ -0,0 +1,23 @@
+# Data Processing
+
+#Import the Libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Import the dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,3].values
+
+#Splitting the Training set and Test Set
+
+from sklearn.model_selection import train_test_split 
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, random_state = 0)
+
+#Feature Scaling
+'''from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train =  sc_X.fit_transform(X_train)
+X_test =  sc_X.transform(X_test)'''
